acada417d605fc2d6ba14484252e7712ac0ddb68
Ilia Mediankin <i.mediankin@gmail.com>
2023-06-20 18:38:59 +0300

    feat: add check for anagrams code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// (Проверка на анаграммы равные по длине, игнорируя пробелы)
+ 
+string s1 = "Tom Marvolo Riddle";
+string s2 = "I am Lord Voldemort";
+
+Console.WriteLine(IsAnagrams(s1, s2)? "Строки - анаграммы!" : "Строки не анаграммы.");
+
+
+bool IsAnagrams(string inString1, string inString2){
+    char[] chars1 = (inString1).Replace(" ","").ToLower().ToCharArray();
+    char[] chars2 = (inString2).Replace(" ","").ToLower().ToCharArray();
+    Array.Sort(chars1); string compareS1 = new String(chars1);
+    Array.Sort(chars2); string compareS2 = new String(chars2);
+    return Equals(compareS1, compareS2);
+}
